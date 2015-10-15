c65b6fcd54f2d71774ec04326fedfa41c5484046
schomper <delan.schomper@gmail.com>
2015-10-15 21:45:44 +1000

    Sub topic push accross

--- 6_portal/document_list_generator.py
+++ 6_portal/document_list_generator.py
@@ -1,8 +1,21 @@
 #! /usr/bin/python3
 import sys
+import os
 import json
 
-def process_gamma_file(gamma_file_name):
+HIERARCHY = False
+
+
+def isHierarchy(folder):
+    folder_list = os.listdir(folder)
+
+    if '0' in folder_list:
+        return True
+    else:
+        return False
+
+
+def processSingleGammaFile(gamma_file_name):
     documents_top_topics = []
 
     gamma_file_ptr = open(gamma_file_name, 'r')
@@ -23,27 +36,18 @@ def process_gamma_file(gamma_file_name):
 
     return documents_top_topics
 
-def main():
 
-    overall_dict = {}
+def processSingle(folder):
+    count = 0
+    articles = []
 
-    if len(sys.argv) != 4:
-        print('./document_list_generator <formatted_file> <gamma_file> <output_file>');
-        exit(1)
+    gamma_file_name = folder + '/out/final.gamma'
+    format_file_name = folder + '/initial.formatted'
 
-    format_file_name = sys.argv[1]
-    gamma_file_name = sys.argv[2]
-    output_file_name = sys.argv[3]
-
-    documents_top_topics = process_gamma_file(gamma_file_name)
+    documents_top_topics = processSingleGammaFile(gamma_file_name)
 
     format_file_ptr = open(format_file_name, 'r')
-
     line = format_file_ptr.readline()
-
-    count = 0
-
-    articles = {}
 
     # Process lines
     while line != '':
@@ -52,25 +56,154 @@ def main():
 
         topic_words, date, title, document = line.split('|~|')
 
+        article['doc_id'] = count
         article['date'] = date
-        try:
-            article['top_topics'] = documents_top_topics[count]
-        except:
-            print(count)
-
+        article['top_topics'] = documents_top_topics[count]
         article['title'] = title
 
-        articles[count] = article
+        articles.append(article)
 
         line = format_file_ptr.readline()
         count += 1
 
-    overall_dict['num_articles'] = count
-    overall_dict['articles'] = articles
+    return articles
+
+
+def getTopics(line, zero):
+    line = line.strip()
+    vals = line.split(' ')
+    real_vals = []
+
+    for index in range(len(vals)):
+        if float(vals[index]) > zero + 0.01:
+            real_vals.append(str(index) + ':' + str(vals[index]))
+
+    return real_vals
+
+
+def getZero(gamma_lines):
+    zero = 0
+    lowest_five = []
+
+    for i in range(5):
+        line = gamma_lines[i].strip()
+        vals = line.split(' ')
+
+        numbers = [float(val) for val in vals]
+        sorted_nums = sorted(numbers)
+        lowest_five.append(sorted_nums[0])
+
+    zero = sum(lowest_five) / float(len(lowest_five))
+
+    return zero
+
+
+def order_topics(topics_hash, topic_ids):
+    ordered_topics = []
+
+    for folder in topics_hash:
+        for item in topics_hash[folder]:
+            sub, prob = item.split(':')
+            ordered_topics.append([folder, sub, prob])
+
+    ordered_topics.sort(key=lambda x: float(x[2]), reverse=True)
+
+    ordered_index_topics = []
+
+    for items in ordered_topics:
+        ordered_index_topics.append(topic_ids[items[0]][int(items[1])])
+
+    return ordered_index_topics
+
+
+def processHierarchy(folder):
+    articles = []
+
+    files_list = os.listdir(folder)
+
+    document_hash = {}
+    topic_ids = {}
+
+    for item in files_list:
+        if not item.isdigit():
+            continue
+
+        # Read format file
+        topic_file = folder + '/' + item + '/out/topic_nums.txt'
+        topic_ptr = open(topic_file, 'r')
+        topic_lines = topic_ptr.readlines()
+        topic_ptr.close()
+
+        topic_indexes = []
+        for line in topic_lines:
+            topic_indexes.append(line.strip())
+
+        topic_ids[item] = topic_indexes
+
+        # Read format file
+        format_file = folder + '/' + item + '/initial.formatted'
+        format_ptr = open(format_file, 'r')
+        format_lines = format_ptr.readlines()
+        format_ptr.close()
+
+        # Read gamma file
+        gamma_file = folder + '/' + item + '/out/final.gamma'
+        gamma_ptr = open(gamma_file, 'r')
+        gamma_lines = gamma_ptr.readlines()
+        gamma_ptr.close()
+
+        zero = getZero(gamma_lines)
+
+        # For each document line
+        for index in range(len(format_lines)):
+            article = {}
+            line = format_lines[index].strip()
+            doc_id, prob, topic, date, title, data = line.split('|~|')
+
+            article['doc_id'] = doc_id
+            article['date'] = date
+            article['title'] = title
+
+            if doc_id not in document_hash:
+                document_hash[doc_id] = {}
+                document_hash[doc_id]['topics'] = {}
+
+            document_hash[doc_id]['topics'][item] = \
+                getTopics(gamma_lines[index], zero)
+            document_hash[doc_id]['article'] = article
+
+            if doc_id == "3":
+                print('index ' + str(document_hash[doc_id]['topics']))
+
+    for doc in document_hash:
+        document_hash[doc]['article']['topics'] = \
+            order_topics(document_hash[doc]['topics'], topic_ids)
+
+        articles.append(document_hash[doc]['article'])
+
+    return articles
+
+
+def main():
+    global HIERARCHY
+
+    if len(sys.argv) != 3:
+        print('./document_list_generator <folder> <output_file>')
+        exit(1)
+
+    folder_name = sys.argv[1]
+    output_file_name = sys.argv[2]
+
+    HIERARCHY = isHierarchy(folder_name)
+
+    if HIERARCHY:
+        articles = processHierarchy(folder_name)
+    else:
+        articles = processSingle(folder_name)
 
     # write json out
     output_ptr = open(output_file_name, 'w')
-    output_ptr.write(json.dumps(overall_dict, sort_keys=True, indent=4))
+    output_ptr.write(json.dumps(articles, sort_keys=True, indent=4))
     output_ptr.close()
 
 if __name__ == '__main__':
--- 6_portal/relevant_topic_words.py
+++ 6_portal/relevant_topic_words.py
@@ -2,21 +2,16 @@
 import sys
 import json
 
-def main():
+count = 0
 
-    if len(sys.argv) != 5:
-        # TODO make this meaningful
-        print('./relevant_topic_words.py <beta_file> <vocab_file> <number_of_words_per_topic> <output_file>')
-        exit(1)
 
-    input_file = sys.argv[1]
-    vocab_file = sys.argv[2]
-    num_words = int(sys.argv[3])
-    output_file = sys.argv[4]
-
-    #--- Add vocab to memory ---#
+def process_folder(folder, num_words, parent):
+    global count
     vocab = []
-    vocab_ptr = open(vocab_file, 'r')
+    topics = []
+
+    # --- Add vocab to memory --- #
+    vocab_ptr = open(folder + '/initial.vocab', 'r')
     line = vocab_ptr.readline()
 
     while line != '':
@@ -25,11 +20,12 @@ def main():
 
     vocab_ptr.close()
 
-    topics = []
     # for each line in the beta file
     indices = list(range(len(vocab)))
 
-    for topic in open(input_file, 'r'):
+    out = open(folder + '/out/topic_nums.txt', 'w')
+    for topic in open(folder + '/out/final.beta', 'r'):
+        topic_hash = {}
         topic_words = []
 
         topic = list(map(float, topic.split()))
@@ -38,12 +34,44 @@ def main():
         for i in range(num_words):
             topic_words.append(vocab[indices[i]])
 
-        topics.append(topic_words)
+        topic_hash['index'] = count
+        topic_hash['label'] = count
+        topic_hash['words'] = topic_words
+        topic_hash['parent'] = parent
+
+        out.write(str(count) + '\n')
+        topics.append(topic_hash)
+        count += 1
+
+    return topics
 
 
-    #--- Write to file ---#
+def main():
+    if len(sys.argv) != 5:
+        print('./relevant_topic_words.py <folder> ' +
+              '<number_of_words_per_topic> <num_subs> <output_file>')
+        exit(1)
+
+    input_folder = sys.argv[1]
+    num_words = int(sys.argv[2])
+    num_subs = int(sys.argv[3])
+    output_file = sys.argv[4]
+
+    topic_db_object = []
+
+    # Process highest level
+    folder = input_folder
+    parent = -1
+    topic_db_object.extend(process_folder(folder, num_words, parent))
+
+    for i in range(0, num_subs):
+        parent = i
+        folder = input_folder + '/' + str(i)
+        topic_db_object.extend(process_folder(folder, num_words, parent))
+
+    # --- Write to file --- #
     output_ptr = open(output_file, 'w')
-    output_ptr.write(json.dumps(topics, sort_keys=True, indent=4)) #TODO
+    output_ptr.write(json.dumps(topic_db_object, sort_keys=True, indent=4))
     output_ptr.close()
 
 
