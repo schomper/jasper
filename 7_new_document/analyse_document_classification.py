cdbf326f7876c12962484486e79bd47caf28c619
schomper <delan.schomper@gmail.com>
2015-10-18 12:09:48 +1000

    Additional analisys tools and bug fix

--- 7_new_document/analyse_document_classification.py
+++ 7_new_document/analyse_document_classification.py
@@ -29,8 +29,14 @@ def classify(contents, vocab_details, vocab_index):
             document_hash[topic] += \
                 vocab_details[word_index]['topics'][topic] / word_total
 
+    keys = []
     for key in document_hash:
-        topics.append(document_hash[key])
+        keys.append(key)
+    keys.sort()
+
+    topics = list(range(len(keys)))
+    for index in range(len(keys)):
+        topics[index] = document_hash[keys[index]]
 
     indices = list(range(len(topics)))
     indices = sorted(indices, key=lambda x: -topics[x])
@@ -38,7 +44,7 @@ def classify(contents, vocab_details, vocab_index):
     return indices
 
 
-def compare_topics(classed, known, amount):
+def compare_topics_order(classed, known, amount):
     """ (list, list, int) -> boolean
 
     Returns true the number of elements in amount are in the correct order
@@ -53,9 +59,53 @@ def compare_topics(classed, known, amount):
     return correct
 
 
+def compare_topic_slice(classed, known, cutoff):
+    """ (list, list, int) -> boolean
+
+    Returns true if both lists contain the same items up to the cutoff
+    """
+
+    classed = classed[:cutoff]
+    known = known[:cutoff]
+
+    known.sort()
+    classed.sort()
+
+    for i in range(cutoff):
+        if i == len(known):
+            break
+        if i == len(classed):
+            break
+        if str(known[i]) != str(classed[i]):
+            return False
+
+    return True
+
+
+def compare_position_offset(classed, known, offset):
+    """ (list, list, int, int) -> boolean
+
+    Returns true if the elements within the list
+    are never more than offset positions away from each other
+    """
+    num_topics = 0
+
+    for index in range(num_topics):
+        if index == len(known):
+            break
+        if index == len(classed):
+            break
+
+        if str(known[index]) != str(classed[index]):
+            return False
+
+    return True
+
+
 def main():
     check_start('./test_doc_guess_algorithm.py <folder> <output>', 3)
-    top_count = 0
+    topic_order_count = 0
+    topic_feeling_count = 0
 
     # Input naming
     folder = sys.argv[1]
@@ -91,10 +141,27 @@ def main():
         document_contents = clean_line(document_contents)
         classed_doc = classify(document_contents, vocab_details, vocab_index)
 
-        if compare_topics(classed_doc, known_topics, 1):
-            top_count = 1 + top_count
+        if len(classed_doc) == 0:
+            print(document_contents)
+            continue
 
-    print('Topics Correct: ' + str(top_count / num_docs))
+        if compare_topics_order(classed_doc, known_topics, 1):
+            topic_order_count += 1
+
+        if compare_topic_slice(classed_doc, known_topics, 3):
+            topic_feeling_count += 1
+
+        if index % 100 == 0:
+            print(str(index))
+
+    out_ptr = open(output, 'w')
+    out_ptr.write('Number of documents: ' + str(num_docs) + '\n')
+    out_ptr.write('Topics Correct: %s\n' % str(topic_order_count / num_docs))
+
+    print('Number of documents: ' + str(num_docs))
+    print('Topics Correct: ' + str(topic_order_count / num_docs))
+    print('Topics Kinda Right: ' + str(topic_feeling_count / num_docs))
+    print('\a')
 
 if __name__ == '__main__':
     main()
